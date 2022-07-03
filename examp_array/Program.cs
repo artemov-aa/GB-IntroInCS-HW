f50528b426529114b53994eb8f4c5e04843a5978
Alexey.Artemov <lexart.pub@ya.ru>
2022-07-03 19:49:17 +0500

    Renamed files

---
+++ examp_array/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array; //объявляем целочисленный массив
+int size = 0; //это будет размер нашего массива
+
+Console.Write("Введите размер массива: ");
+size = Convert.ToInt32(Console.ReadLine());
+array = new int[size]; // выделяем память для нашего массива
+
+// далее вводим массив поэлементно
+for (int i = 0; i < size; i++)
+{
+    Console.Write("Введите элемент № ");
+    Console.WriteLine(Convert.ToString(i));
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
