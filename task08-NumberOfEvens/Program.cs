7df2f94da040a1e4d2c06f6b4d81e372f727497f
Alexey.Artemov <lexart.pub@ya.ru>
2022-07-03 19:22:58 +0500

    Added Task08 "Number Of Even Numbers"

---
+++ task08-NumberOfEvens/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 8.
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+
+int num = 0;
+
+Console.WriteLine("Введите число: ");
+num = Int32.Parse(Console.ReadLine());
+
+
+Console.Write($"Вот все четные числа в ряду от 1 до {num}: ");
+
+for (int i = 1; i <= num; i++)
+{
+    if ((i % 2) == 0)
+        Console.Write($"{i} ");
+}
+
+Console.WriteLine();
