6eff601c8e39c654a71cf5a554fc0001ed840b65
Alexey.Artemov <lexart.pub@ya.ru>
2022-07-03 18:22:50 +0500

    Added Task03 "Maximum of three numbers"

---
+++ task03-MaxOfThree/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+ int num = 0;
+ int maxNum = 0;
+
+ for (int i = 1; i < 4; i++)
+ {
+    Console.WriteLine($"Введите число № {i}");
+    num = Int32.Parse(Console.ReadLine());
+    if (num > maxNum)
+        maxNum = num;
+ }
+
+ Console.WriteLine($"Максимальное число, введенное вами: {maxNum}");
