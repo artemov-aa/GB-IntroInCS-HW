20a99016d1b58da31fd399c65002250875b4d960
Alexey.Artemov <lexart.pub@ya.ru>
2022-07-24 01:14:57 +0500

    Modified Task58 Added new methods and modified existing.

--- task58/task58.cs
+++ task58/task58.cs
@@ -1,39 +1,71 @@
 ﻿// Задача 58
 // Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 // Например, заданы 2 массива:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// 5 2 6 7
-// и
-// 1 5 8 5
-// 4 9 4 2
-// 7 2 2 6
-// 2 3 4 7
-// Их произведение будет равно следующему массиву:
-// 1 20 56 10
-// 20 81 8 6
-// 56 8 4 24
-// 10 6 24 49
 
-int VerifyIntegerNumber(string input)
+Console.Clear();
+PrintTitle("Задача 58. Произведение матриц");
+
+int rowsFirstMatrix        = InputNumber("Введите количество строк матрицы A: ", 0);
+int columnsFirstMatrix     = InputNumber("Введите количество столбцов матрицы A: ", 0);
+int rowsSecondMatrix       = InputNumber("Введите количество строк матрицы B: ", 0);
+int columnsSecondMatrix    = InputNumber("Введите количество столбцов матрицы B: ", 0);
+
+int[,] firstMatrix  = CreateMatrix(rowsFirstMatrix, columnsFirstMatrix);
+int[,] secondMatrix = CreateMatrix(rowsSecondMatrix, columnsSecondMatrix);
+
+Console.WriteLine("\nИсходные матрицы:\n");
+PrintMatrix(firstMatrix);
+PrintMatrix(secondMatrix);
+
+int[,] productOfMatrices;
+if (!MatrixProduct(firstMatrix, secondMatrix, out productOfMatrices))
+    Console.WriteLine("Матрицы А и В не согласованы и не могут быть перемножены.");
+else
 {
-    int result;
-
-    while (!int.TryParse(input, out result))
-    {
-        Console.Write("Неверный ввод! \nВведите натуральное число -> ");
-        input = Console.ReadLine();
-    }
-
-    return result;
+    Console.WriteLine("Произведение матриц А и В:\n");
+    PrintMatrix(productOfMatrices);
 }
 
-int[,] GetMatrix(int rows = 4, int columns = 4, int min = 0, int max = 9)
+Console.WriteLine();
+
+
+
+
+void PrintTitle(string message)
+{
+    string underline = new string('-', message.Length);
+    Console.WriteLine($"{message}\n{underline}\n");
+}
+
+int InputNumber(string message, int typeOfNumber)
+{
+    Console.Write(message);
+    string input = Console.ReadLine();
+    int number = 0;
+    switch (typeOfNumber)
+    {
+        case 0:
+            while (!int.TryParse(input, out number) || number < 1)
+            {
+                Console.Write("Неверный ввод!\nВведите натуральное число: ");
+                input = Console.ReadLine();
+            }
+            break;
+        case 1:
+            while (!int.TryParse(input, out number))
+            {
+                Console.Write("Неверный ввод!\nВведите целое число: ");
+                input = Console.ReadLine();
+            }
+            break;
+    }
+    return number;
+}
+
+int[,] CreateMatrix(int rows = 4, int columns = 4, int min = 0, int max = 10)
 {
     int[,] matrix = new int[rows, columns];
     var rnd = new Random();
-
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < columns; j++)
@@ -41,15 +73,14 @@ int[,] GetMatrix(int rows = 4, int columns = 4, int min = 0, int max = 9)
             matrix[i, j] = rnd.Next(min, max + 1);
         }
     }
-
     return matrix;
 }
 
 void PrintMatrix(int[,] matrix)
 {
-    int rowsLength = matrix.GetLength(0), columnsLength = matrix.GetLength(1);
+    int rowsLength = matrix.GetLength(0);
+    int columnsLength = matrix.GetLength(1);
     const int widthOfColumn = 3;
-
     for (int i = 0; i < rowsLength; i++)
     {
         for (int j = 0; j < columnsLength; j++)
@@ -59,7 +90,6 @@ void PrintMatrix(int[,] matrix)
         }
         Console.WriteLine();
     }
-
     Console.WriteLine();
 }
 
@@ -69,51 +99,16 @@ bool MatrixProduct(int[,] mtrxA, int[,] mtrxB, out int[,] mtrxProduct)
     int colLenMtrxA = mtrxA.GetLength(1), colLenMtrxB = mtrxB.GetLength(1);
     mtrxProduct = new int[rowLenMtrxA, colLenMtrxB];
     int element = 0;
-
     if (colLenMtrxA != rowLenMtrxB) return false;
-
     for (int i = 0; i < rowLenMtrxA; i++)
-    {
         for (int j = 0; j < colLenMtrxB; j++)
         {
             for (int k = 0; k < colLenMtrxA; k++)
             {
                 element += mtrxA[i, k] * mtrxB[k, j];
             }
-
             mtrxProduct[i, j] = element;
             element = 0;
         }
-    }
-
     return true;
-}
-
-//--------------------------------------------------------------------//
-
-Console.Clear();
-
-Console.Write("Задайте количество строк матрицы A: ");
-int rowsA = VerifyIntegerNumber(Console.ReadLine());
-Console.Write("Задайте количество столбцов матрицы A: ");
-int columnsA = VerifyIntegerNumber(Console.ReadLine());
-Console.Write("Задайте количество строк матрицы B: ");
-int rowsB = VerifyIntegerNumber(Console.ReadLine());
-Console.Write("Задайте количество столбцов матрицы B: ");
-int columnsB = VerifyIntegerNumber(Console.ReadLine());
-
-int[,] matrixA = GetMatrix(rowsA, columnsA);
-int[,] matrixB = GetMatrix(rowsB, columnsB);
-
-Console.WriteLine("\nИсходные матрицы:\n");
-PrintMatrix(matrixA);
-PrintMatrix(matrixB);
-
-int[,] productOfMatrices;
-if (!MatrixProduct(matrixA, matrixB, out productOfMatrices))
-    Console.WriteLine("Матрицы А и В не согласованы и не могут быть перемножены.");
-else
-{
-    Console.WriteLine("Произведение матриц А и В:\n");
-    PrintMatrix(productOfMatrices);
 }
